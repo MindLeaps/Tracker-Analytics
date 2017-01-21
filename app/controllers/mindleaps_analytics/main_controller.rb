2f76a6920fe3a492670eabca8f33c97d49941fca
sz <(sz@zaaz.nl)>
2017-01-21 18:11:53 +0200

    Simplified stuff, more comments

--- app/controllers/mindleaps_analytics/main_controller.rb
+++ app/controllers/mindleaps_analytics/main_controller.rb
@@ -4,29 +4,22 @@ module MindleapsAnalytics
   class MainController < ApplicationController
     def first
       now = Date.today
-      # Student.group(:group_id)
       students = Student.all
-      @x = Group.all
 
       # let's create a hash with key group_id and data an empty array which will contain the points (x,y)
       # we do this so we can create a data series per group in the scatter chart
-      @groups = {}
-      group_ids = students.group(:group_id).count.keys
-      group_ids.each do |group_id|
-        @groups[group_id.to_s.to_sym] = []
-      end
+      groups = Hash.new
 
       # now calculate the performance for each student
       students.each do |student|
 
         id = student.id
+        # The number of lessons the student has attended
         nr_of_lessons = Grade.where(student_id: id).distinct.count(:lesson_id)
         dob = student.dob
         age = now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
 
-        point = []
-
-        # Patrick's formula
+        # Non-linear multivariate regression formula
         performance = 3.31
         performance += 1.72*(10**-2) * nr_of_lessons
         performance += -8.14*(10**-5) * nr_of_lessons**2
@@ -43,17 +36,21 @@ module MindleapsAnalytics
         # performance += -0.169 if student.data_collector_6
         # performance += -0.06 if student.class_type_2
 
+        point = []
         point << nr_of_lessons
         point << performance
 
         # add this point to the correct (meaning: for this student's Group) data series
-        @groups[student.group_id.to_s.to_sym] << point
+        if groups[student.group_id.to_s.to_sym] == nil
+          groups[student.group_id.to_s.to_sym] = []
+        end
+        groups[student.group_id.to_s.to_sym] << point
 
       end
 
-      # now get the groupname for each group, and store groupname and data together in an array
+      # now get the group name for each group, and store group name and data together in an array
       @series = []
-      @groups.each do |key, data|
+      groups.each do |key, data|
         group = Group.find(key.to_s.to_i)
         @series << {name: group.group_name, data: data}
       end
