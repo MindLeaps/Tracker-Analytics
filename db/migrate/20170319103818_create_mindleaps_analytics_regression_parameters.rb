9de98b23f9dd68c9112b6dd6405610c637ede213
Tomislav Pesut <tomca32@gmail.com>
2017-05-21 21:38:46 -0400

    TRACK-118 - Hardcoded Performance Regression parameters

--- db/migrate/20170319103818_create_mindleaps_analytics_regression_parameters.rb
+++ db/migrate/20170319103818_create_mindleaps_analytics_regression_parameters.rb
@@ -1,10 +1,10 @@
-class CreateMindleapsAnalyticsRegressionParameters < ActiveRecord::Migration[5.0]
-  def change
-    create_table :mindleaps_analytics_regression_parameters do |t|
-      t.string :name
-      t.float :value
-
-      t.timestamps
-    end
-  end
-end
+# class CreateMindleapsAnalyticsRegressionParameters < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :mindleaps_analytics_regression_parameters do |t|
+#       t.string :name
+#       t.float :value
+#
+#       t.timestamps
+#     end
+#   end
+# end
