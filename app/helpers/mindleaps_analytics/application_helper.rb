aadce26a08ed6ec1ce9abcbf392bdc80dae1fc73
Tomislav Pesut <tomca32@gmail.com>
2018-01-14 14:21:31 -0500

    Material design and navigation between screens

--- app/helpers/mindleaps_analytics/application_helper.rb
+++ app/helpers/mindleaps_analytics/application_helper.rb
@@ -1,5 +1,37 @@
 module MindleapsAnalytics
   module ApplicationHelper
+    def general_analytics_url
+      url_for controller: 'main', action: :first
+    end
 
+    def subject_analytics_url
+      url_for controller: 'main', action: :second
+    end
+
+    def group_analytics_url
+      url_for controller: 'main', action: :third
+    end
+
+    def general_analytics?
+      current_page?(general_analytics_url) || current_page?(root_url)
+    end
+
+    def subject_analytics?
+      current_page? subject_analytics_url
+    end
+
+    def group_analytics?
+      current_page? group_analytics_url
+    end
+
+    def link_to_disable_if_current(url)
+      return '' if current_page? url
+
+      "href=#{url}"
+    end
+
+    def href_to url
+      "href=#{url}"
+    end
   end
 end
